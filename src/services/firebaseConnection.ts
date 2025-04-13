1dc0b739d6ed73c42e831f6f32a43f600aa40836
joenog <joabnogueira@outlook.com>
2025-04-13 10:43:29 -0300

    secure connection keys

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -2,6 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_I,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db }
+
+
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyCNC_lIVHsdcL3lmF8HL_7o9D02psM0Chg",
   authDomain: "treelink-5c4e0.firebaseapp.com",
@@ -11,8 +30,4 @@ const firebaseConfig = {
   appId: "1:346048825329:web:1ab8b20cdd1dd4ac986650"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db }
+*/
